c768a02b54121be8b9bb5b386c39e60c81ee7719
mdx97 <mathewhorner123@gmail.com>
2020-07-10 22:33:50 -0500

    Organize constants file.

--- includes/Constants.h
+++ includes/Constants.h
@@ -2,16 +2,20 @@
 #define CONSTANTS_H
 
 #include <string>
+#include "SDL.h"
 
 namespace Constants {
-    // todo: organize.
     const std::string TITLE = "Mason Simulator '20";
+
     const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
+
+    const std::string LOG_FILE_PATH = "log.txt";
+
     const std::string THEME_SONG_PATH = "resources/theme.wav";
+
     const std::string PLAY_BUTTON_PATH = "resources/PlayButton.bmp";
     const std::string PLAY_BUTTON_HOVER_PATH = "resources/PlayButtonHover.bmp";
-    const std::string LOG_FILE_PATH = "log.txt";
     const std::string PLAYING_AREA_PATH = "resources/PlayingArea.bmp";
 };
 
